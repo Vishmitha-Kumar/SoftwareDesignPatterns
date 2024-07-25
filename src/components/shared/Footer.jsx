8ea0037381a26bc47580cef7e43b372a7536f616
Vishmitha-Kumar <vishmithak05@gmail.com>
2024-07-26 00:31:37 +0530

    home

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -4,11 +4,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="w-full py-8 border-t-2 border-primary/50 bg-black">
+    <footer className="w-full py-8 border-t-2 border-primary/50 bg-neutral-500">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-start">
         <div className="w-full md:w-1/4 mb-8 md:mb-0">
-          <h2 className="text-primary font-bold text-4xl mb-4">HALLHUNT</h2>
-          <ul className="text-primary">
+          <h2 className="text-primary font-extrabold text-4xl mb-4">HALLHUNT</h2>
+          <ul className="text-white">
             <li className="mb-2">About Us</li>
             <li className="mb-2">Contact</li>
             <li className="mb-2">Join Our Team</li>
@@ -16,8 +16,8 @@ const Footer = () => {
           </ul>
         </div>
         <div className="w-full md:w-1/4 mb-8 md:mb-0">
-          <h2 className="text-primary font-bold text-lg mb-4">Services</h2>
-          <ul className="text-primary">
+          <h2 className="text-white font-bold text-lg mb-4">Services</h2>
+          <ul className="text-white">
             <li className="mb-2">Wedding Hall</li>
             <li className="mb-2">Party Hall</li>
             <li className="mb-2">Function Hall</li>
@@ -25,8 +25,8 @@ const Footer = () => {
           </ul>
         </div>
         <div className="w-full md:w-1/4 mb-8 md:mb-0">
-          <h2 className="text-primary font-bold text-lg mb-4">Help & FAQs</h2>
-          <ul className="text-primary">
+          <h2 className="text-white font-bold text-lg mb-4">Help & FAQs</h2>
+          <ul className="text-white">
             <li className="mb-2">Contact Us</li>
             <li className="mb-2">FAQ</li>
             <li className="mb-2">Resources</li>
@@ -34,8 +34,8 @@ const Footer = () => {
           </ul>
         </div>
         <div className="w-full md:w-1/4 mb-8 md:mb-0">
-          <h2 className="text-primary font-bold text-lg mb-4">Business</h2>
-          <ul className="text-primary">
+          <h2 className="text-white font-bold text-lg mb-4">Business</h2>
+          <ul className="text-white">
             <li className="mb-2">List your venue</li>
             <li className="mb-2">List your service</li>
            
@@ -43,17 +43,17 @@ const Footer = () => {
         </div>
        
         <div className="w-full md:w-1/4 flex justify-start md:justify-end items-center">
-        <h2 className="text-primary font-bold text-lg mb-4">Keep in touch with us</h2>
-          <a href="#" target="_blank" className="h-8 w-8 rounded-md text-primary hover:bg-primary/50 hover:text-background flex justify-center items-center">
+        <h2 className="text-white font-bold text-lg mb-4">Keep in touch with us</h2>
+          <a href="#" target="_blank" className="h-8 w-8 rounded-md text-white hover:bg-primary/50 hover:text-background flex justify-center items-center">
             <Facebook className="h-6 w-6" />
           </a>
-          <a href="#" target="_blank" className="h-8 w-8 rounded-md text-primary hover:bg-primary/50 hover:text-background flex justify-center items-center">
+          <a href="#" target="_blank" className="h-8 w-8 rounded-md text-white hover:bg-primary/50 hover:text-background flex justify-center items-center">
             <Twitter className="h-6 w-6" />
           </a>
-          <a href="#" target="_blank" className="h-8 w-8 rounded-md text-primary hover:bg-primary/50 hover:text-background flex justify-center items-center">
+          <a href="#" target="_blank" className="h-8 w-8 rounded-md text-white hover:bg-primary/50 hover:text-background flex justify-center items-center">
             <Instagram className="h-6 w-6" />
           </a>
-          <a href="#" target="_blank" className="h-8 w-8 rounded-md text-primary hover:bg-primary/50 hover:text-background flex justify-center items-center">
+          <a href="#" target="_blank" className="h-8 w-8 rounded-md text-white hover:bg-primary/50 hover:text-background flex justify-center items-center">
             <Youtube className="h-6 w-6" />
           </a>
         </div>
